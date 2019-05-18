34d532938f0f18069db2d1b240b34544725c4176
Emeka <mekzycodes@gmail.com>
2019-05-18 06:54:16 +0100

    #166102354 Setup Postgres Database

---
+++ server/models/db.js
@@ -0,0 +1,19 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+let pool;
+if (process.env.NODE_ENV === 'test') {
+  pool = new Pool({ connectionString: process.env.TESTDB_URL });
+} else {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+}
+
+const db = {
+  query: (text, params) => pool.query(text, params),
+};
+
+export default db;
